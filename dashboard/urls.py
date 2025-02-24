f044a70c3733ba6374e5f5f20024948fca710ff3
Athailah <tuxkeren@gmail.com>
2025-02-24 13:37:38 +0700

    update kode

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from report import views
 
 
 urlpatterns = [
---
+++ report/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Machine(models.Model):
+    name = models.CharField(max_length=100)
+    note = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Report(models.Model):
+    param_1 = models.DecimalField(max_digits=10, decimal_places=2)
+    param_2 = models.DecimalField(max_digits=10, decimal_places=2)
+    param_3 = models.DecimalField(max_digits=10, decimal_places=2)
+    param_4 = models.DecimalField(max_digits=10, decimal_places=2)
+    param_5 = models.DecimalField(max_digits=10, decimal_places=2)
+    note = models.TextField()
+    machine = models.ForeignKey('Machine', on_delete=models.CASCADE)
+    
+
