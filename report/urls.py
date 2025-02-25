f5723e3762ea6a911a8b9d043952290a86721d49
Athailah <tuxkeren@gmail.com>
2025-02-25 14:42:16 +0700

    update code

--- report/urls.py
+++ report/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='dashbord'),
-    path('machines/', list_machine, name='daftar_mesin')
+    path('machines/', list_machine, name='daftar-mesin'),
+    path('entry_data/', entry_data, name='entri-data')
     
 ]
--- report/views.py
+++ report/views.py
@@ -6,6 +6,10 @@ def index(request):
     return render(request, 'dashboard.html')
 
 
+def entry_data(request):
+    return render(request, 'entry_data.html')
+
+
 def list_machine(request):
     machines = Machine.objects.all()
     return render(request, 'machine_list.html', {'machines': machines})
