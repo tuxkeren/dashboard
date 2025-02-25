03595e0b5b7df3ef11fd5c2a72296e9e42fad909
Athailah <tuxkeren@gmail.com>
2025-02-25 16:17:30 +0700

    update code

--- report/models.py
+++ report/models.py
@@ -14,6 +14,11 @@ class Report(models.Model):
     param_3 = models.DecimalField(max_digits=10, decimal_places=2)
     param_4 = models.DecimalField(max_digits=10, decimal_places=2)
     param_5 = models.DecimalField(max_digits=10, decimal_places=2)
+    param_6 = models.CharField(max_length=50)
+    param_7 = models.CharField(max_length=50)
+    param_8 = models.CharField(max_length=50)
+    param_9 = models.CharField(max_length=50)
+    param_0 = models.CharField(max_length=50)
     note = models.TextField()
     machine = models.ForeignKey('Machine', on_delete=models.CASCADE)
     
